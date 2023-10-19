62e45ffd9cdbd5c537babc74aac16fb3fe18e5c0
Aleksandr Usolcev <alexusolev@gmail.com>
2023-10-19 09:35:39 +0300

    feat(consumer): base initial

---
+++ consumer/config.py
@@ -0,0 +1,30 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    RABBITMQ_USER: str = 'username'
+    RABBITMQ_PASS: str = 'password'
+    RABBITMQ_HOST: str = '127.0.0.1'
+    RABBITMQ_QUEUE_NAME: str = 'queue_name'
+
+    WS_HOST: str = '127.0.0.1'
+    WS_PORT: int = 8001
+
+    @property
+    def get_amqp_url(self):
+        return (
+            f'amqp://{self.RABBITMQ_USER}:'
+            f'{self.RABBITMQ_PASS}@{self.RABBITMQ_HOST}/'
+        )
+
+    @property
+    def get_ws_url(self):
+        return f'ws://{self.WS_HOST}:{self.WS_PORT}/listen_results'
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8'
+    )
+
+
+settings = Settings()
---
+++ consumer/main.py
@@ -0,0 +1,48 @@
+import asyncio
+import json
+from typing import Dict
+
+import aio_pika
+import aio_pika.abc
+import websockets
+
+from config import settings
+
+
+async def reverse_text(text: str) -> Dict[str, str]:
+    reversed_text = text[::-1]
+    return json.dumps({'reversed_text': reversed_text})
+
+
+async def send_result_to_websocket(data: Dict[str, str]):
+    async with websockets.connect(settings.get_ws_url) as ws:
+        await ws.send(data)
+
+
+async def main(loop):
+    connection = await aio_pika.connect_robust(
+        settings.get_amqp_url,
+        loop=loop
+    )
+
+    async with connection:
+        queue_name = settings.RABBITMQ_QUEUE_NAME
+        channel: aio_pika.abc.AbstractChannel = await connection.channel()
+        queue: aio_pika.abc.AbstractQueue = await channel.declare_queue(
+            queue_name,
+            auto_delete=True
+        )
+
+        async with queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                async with message.process():
+                    text = message.body.decode()
+                    data = await reverse_text(text)
+                    print(data)
+                    if queue.name in message.body.decode():
+                        break
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
+    loop.close()
