0cb5d43473ec4ef570129bfd3a0fc04c6335c6c8
Aleksandr Usolcev <alexusolev@gmail.com>
2023-10-24 16:41:53 +0300

    refactor(consumer): class-based worker

--- consumer/main.py
+++ consumer/main.py
@@ -1,22 +1,19 @@
 import asyncio
 
 from internal.config import settings
-from internal.logging import logger
-from worker import consume
+from services.text import reverse_text
+from worker import Worker
 
 
 async def main():
-    while True:
-        try:
-            await consume(
-                loop=loop,
-                amqp_url=settings.get_amqp_url,
-                ws_url=settings.get_ws_url,
-                queue_name=settings.RABBITMQ_QUEUE_NAME,
-            )
-        except Exception as e:
-            logger.error(e)
-            await asyncio.sleep(settings.CONSUMER_RECONNECT_TIMEOUT)
+    consumer = Worker(
+        amqp_url=settings.get_amqp_url,
+        ws_url=settings.get_ws_url,
+        queue_name=settings.RABBITMQ_QUEUE_NAME,
+        worker_func=reverse_text,
+        timeout=settings.CONSUMER_RECONNECT_TIMEOUT,
+    )
+    await consumer.run()
 
 
 if __name__ == '__main__':
--- consumer/worker.py
+++ consumer/worker.py
@@ -1,41 +1,76 @@
+import asyncio
+
 import aio_pika
 import aio_pika.abc
 import websockets
 from websockets.exceptions import ConnectionClosedError
 
 from internal.logging import logger
-from services.text import reverse_text
 
 
-async def queue_processing(queue, ws):
-    async with queue.iterator() as queue_iter:
-        async for message in queue_iter:
-            async with message.process():
-                text = message.body.decode()
-                data = await reverse_text(text)
-                await ws.send(data)
-                if queue.name in message.body.decode():
-                    break
+class Worker:
 
+    def __init__(
+        self,
+        amqp_url: str,
+        ws_url: str,
+        queue_name: str,
+        worker_func: callable,
+        timeout: int = 5
+    ):
+        self.amqp_url = amqp_url
+        self.ws_url = ws_url
+        self.queue_name = queue_name
+        self.worker_func = worker_func
+        self.timeout = timeout
 
-async def websocket_connection(queue, url):
-    async with websockets.connect(url) as ws:
-        try:
-            logger.info(f'WebSocket connection: {url}')
-            await queue_processing(queue, ws)
-        except (ConnectionClosedError, ConnectionRefusedError):
-            logger.error(
-                'WebSocket connection closed unexpectedly. Reconnecting...'
-            )
+        self.loop = asyncio.get_event_loop()
+        self.ws = None
 
+    async def websocket_connection(self):
+        while True:
+            try:
+                self.ws = await websockets.connect(self.ws_url)
+                logger.info(f'WebSocket connection: {self.ws_url}')
+                return
+            except (ConnectionClosedError, ConnectionRefusedError):
+                logger.error(
+                    'WebSocket connection closed unexpectedly. '
+                    'Reconnecting...'
+                )
+                await asyncio.sleep(self.timeout)
 
-async def consume(loop, amqp_url, ws_url, queue_name):
-    connection = await aio_pika.connect_robust(amqp_url, loop=loop)
-    async with connection:
-        queue_name = queue_name
-        channel: aio_pika.abc.AbstractChannel = await connection.channel()
-        queue: aio_pika.abc.AbstractQueue = await channel.declare_queue(
-            queue_name,
-            durable=True,
+    async def process_message(self, message):
+        async with message.process():
+            text = message.body.decode()
+            data = await self.worker_func(text)
+            await self.ws.send(data)
+            if self.queue_name in message.body.decode():
+                return True
+
+    async def queue_processing(self, queue):
+        async with queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                await self.process_message(message)
+
+    async def consume(self):
+        connection = await aio_pika.connect_robust(
+            self.amqp_url,
+            loop=self.loop
         )
-        await websocket_connection(queue, ws_url)
+        async with connection:
+            channel: aio_pika.abc.AbstractChannel = await connection.channel()
+            queue: aio_pika.abc.AbstractQueue = await channel.declare_queue(
+                self.queue_name,
+                durable=True,
+            )
+            await self.websocket_connection()
+            await self.queue_processing(queue)
+
+    async def run(self):
+        while True:
+            try:
+                await self.consume()
+            except Exception as e:
+                logger.error(e)
+                await asyncio.sleep(self.timeout)
